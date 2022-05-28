d3f307a6e06bcfc980dc5d59a635ba8a08a9d557
taegukang35 <67684178+taegukang35@users.noreply.github.com>
2022-05-28 17:56:19 +0900

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# SVM에 사용할 데이터 만들기: (7,0), (0,7)에 몰리도록 생성
+# SVM에 사용할 데이터 만들기
+# 50개씩 (7,0), (0,7)에 몰리도록 랜덤하게 생성
+# (0,7)에 가까운 점은 S_i = 1, (7,0)에 가까운 점은 S_i = -1
 n = 100
 r1,r2 = np.random.randn(n//2,1),np.random.randn(n//2,1)+7
 s1,s2 = np.ones((n//2,1)),-np.ones((n//2,1))
@@ -11,45 +13,50 @@ points = np.hstack((S,r))
 X,Y = np.array(points[:,1]),np.array(points[:,2])
 X,Y = X.reshape(n,1), Y.reshape(n,1)
 
-# 초기값 세팅
-mu = np.ones((n,1))
-w = np.vstack((-1,1,0,mu)) # w=[a,b,c,mu]^T
+# 초기값 세팅: 원래는 PhaseⅠ Method로 찾아야 함
+mu = np.ones((n,1)) # mu는 lagrangian multiplier
+w = np.vstack((-1,1,0,mu)) # w=[a,b,c,mu]^T를 매 스텝마다 업데이트
 t = 1
 
-# Dual Interior Point Method; Newton Method로 zero-finding
+# Dual Interior Point Method
 for k in range(50):
-    # a,b,c,mu 를 업데이트 해야함
-    t = 0.9*t
-    a,b,c,mu = w[0],w[1],w[2],w[3:]
-    print(a,b,c)
+    # SVM 라그랑지안 식: L(x,mu) = 1/2[a,b]@[a,b]^T + sigma(mu_i*g_i)
+    # Dual IPM: delta f + mu@delta g = 0, mu*g + t = 0 (t->0) 
+    # 이를 만족하는 a,b,c,mu 를 Newton Method 로 찾아야함 
 
-    # R(w) 구하기
+    t = 0.9*t #t->0
+    a,b,c,mu = w[0],w[1],w[2],w[3:]
+    print(a, b, c)
+
+    # g(w): g_i = si(c-ax-by)+1 
+    g = c - np.array((a, b)).T @ np.vstack((X.T, Y.T))
+    g = S * g.T + 1
+
+    # R(w) 계산
     dg_dx = np.hstack((-S*X,-S*Y,S))
-    R1 = np.vstack((a,b,0))+np.transpose(dg_dx)@mu
-    # g_i = si(c-ax-by)+1
-    g = c-np.array((a,b)).T@np.vstack((np.transpose(X),np.transpose(Y)))
-    g = S*np.transpose(g)+1
+    R1 = np.vstack((a,b,0))+dg_dx.T @ mu
     R2 = mu*g+t*np.ones((n,1))
     R = np.vstack((R1,R2))
 
-    # dR_dw 구하기
-    dR_dw1 = np.array([[1,0,0],[0,1,0],[0,0,0]])
-    dR_dw2 = np.transpose(dg_dx)
-    dR_dw3 = np.diag(np.squeeze(mu))@dg_dx
-    dR_dw4 = np.diag(np.squeeze(g))
-    dR_dw = np.block([[dR_dw1,dR_dw2],[dR_dw3,dR_dw4]])
+    # dR/dw 계산
+    dR_dw11 = np.array([[1,0,0],[0,1,0],[0,0,0]])
+    dR_dw12 = dg_dx.T
+    dR_dw21 = np.diag(np.squeeze(mu))@dg_dx
+    dR_dw22 = np.diag(np.squeeze(g))
+    dR_dw = np.block([[dR_dw11,dR_dw12],[dR_dw21,dR_dw22]])
 
     # Newton method 로 w 업데이트
+    # Backtracking line search 로 learning rate 구해 더 빠르게 최적화 시킬수도 있음
     w = w-np.linalg.inv(dR_dw)@R
 
-    # 결과 plot 하기
+    # 원래 점들, 결정 경계, 마진 plot 하기 
     xmin, xmax, ymin, ymax = -5, 10, -5, 10
     fig = plt.figure()
     plt.grid()
     plt.scatter(X, Y)
     plt.axis([xmin, xmax, ymin, ymax])
     xx = np.linspace(xmin, xmax, 100)
-    plt.plot(xx, np.squeeze(-(a / b) * xx) + (c / b),'r')
+    plt.plot(xx, np.squeeze(-(a / b) * xx) + (c / b),'r') # 결정경계
     plt.plot(xx, np.squeeze(-(a / b) * xx) + (c + 1 / b),'b')
-    plt.plot(xx, np.squeeze(-(a / b) * xx) + (c - 1 / b),'b')
+    plt.plot(xx, np.squeeze(-(a / b) * xx) + (c - 1 / b),'b') 
     fig.canvas.draw();plt.pause(0.5);fig.canvas.flush_events()
